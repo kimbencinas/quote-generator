243ceabfcf3bf2422e826a78ca8ef06c65e6a4c6
kimbencinas <kimbencinas@gmail.com>
2023-10-04 16:44:38 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import Quotes from './Components/Quotes'
+import Button from './Components/Button'
+import './App.css'
+
+export default function App() {
+  const [quote, setQuote] = useState("");
+
+  const getQuote = () => {
+    fetch('https://zenquotes.io/api/random')
+      .then(response => response.json())
+      .then((data) => {
+        setQuote(data[0].q);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+  
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="content-container">
+          <Quotes quote={quote} />
+          <Button getQuote={getQuote} />
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/Components/Quotes.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react'
+
+export default function Quotes({ quote }) {
+
+    return (
+        <div className="body">
+            <h2 className="quote">{quote}</h2>
+        </div>
+    );
+}
